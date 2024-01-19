feb4c493266368a4a6e05b018a87ed99ec5473e4
Efra Espada <efraespada@gmail.com>
2024-01-19 13:30:30 +0100

    feature: building nice images

--- bin/utils/communicator/service_worker_impl.dart
+++ bin/utils/communicator/service_worker_impl.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:blurhash_dart/blurhash_dart.dart';
 import 'package:image/image.dart';
 
 import 'service_worker.dart';
@@ -19,6 +20,8 @@ class ServiceWorkerImpl extends ServiceWorker {
   @override
   Future<String> processRequest(String requestData) async {
     final data = jsonDecode(requestData);
+
+    final mode = (data['mode'] as String?) ?? 'blank';
 
     final height = data['height'] as int;
     final width = data['width'] as int;
@@ -46,6 +49,7 @@ class ServiceWorkerImpl extends ServiceWorker {
 
     if (height > 0.0 && width > 0.0) {
       await createImage(
+        mode,
         outputFile,
         screenshotData,
         width.toInt(),
@@ -59,14 +63,44 @@ class ServiceWorkerImpl extends ServiceWorker {
   }
 
   Future<void> createImage(
+    String mode,
     String path,
     Uint8List screenshotData,
     int width,
     int height,
   ) async {
-    Image blankImage = Image(width: width, height: height);
+    Image? screenshotImage = decodeImage(screenshotData);
+    if (screenshotImage == null) {
+      return;
+    }
+
+    Image? baseImage;
+
+    // if (mode == 'blurhash') {
+    if (true) {
+      baseImage = createBlurHashImage(width, height, screenshotImage);
+    } else {
+      baseImage = createBlankImage(width, height);
+    }
+
+    if (baseImage == null) {
+      return;
+    }
+
+    screenshotImage = checkImage(baseImage, screenshotImage);
+
+    int x = (width - screenshotImage.width) ~/ 2;
+    int y = (height - screenshotImage.height) ~/ 2;
+
+    drawImage(baseImage, screenshotImage, x, y);
+
+    await File(path).writeAsBytes(encodePng(baseImage));
+  }
+
+  Image? createBlankImage(int width, int height) {
+    final baseImage = Image(width: width, height: height);
     fill(
-      blankImage,
+      baseImage,
       color: ColorRgba8(
         255,
         255,
@@ -74,24 +108,44 @@ class ServiceWorkerImpl extends ServiceWorker {
         255,
       ),
     );
-
-    Image? screenshotImage = decodeImage(screenshotData);
-    if (screenshotImage == null) {
-      return;
-    }
-
-    int x = (width - screenshotImage.width) ~/ 2;
-    int y = (height - screenshotImage.height) ~/ 2;
-
-    drawImage(blankImage, screenshotImage, x, y);
-
-    await File(path).writeAsBytes(encodePng(blankImage));
+    return baseImage;
   }
 
+  Image? createBlurHashImage(int width, int height, Image imageToJoin) {
+    final blurHash = BlurHash.encode(imageToJoin, numCompX: 4, numCompY: 3);
+    return blurHash.toImage(width, height);
+  }
+
+  /// Adjust image size to the base image
+  Image checkImage(Image base, Image imageToJoin) {
+    if (imageToJoin.height < base.height && imageToJoin.width < base.width) {
+      return imageToJoin;
+    }
+
+    if ((imageToJoin.width / base.width) > (imageToJoin.height / base.height)) {
+      int newWidth = base.width;
+      int newHeight =
+          ((newWidth / imageToJoin.width) * imageToJoin.height).round();
+
+      return copyResize(imageToJoin, width: newWidth, height: newHeight);
+    } else {
+      int newHeight = base.height;
+      int newWidth =
+          ((newHeight / imageToJoin.height) * imageToJoin.width).round();
+
+      return copyResize(imageToJoin, width: newWidth, height: newHeight);
+    }
+  }
+
+  /// Draw the image over the base
   void drawImage(Image dst, Image src, int dstX, int dstY) {
     for (int y = 0; y < src.height; y++) {
       for (int x = 0; x < src.width; x++) {
         final color = src.getPixel(x, y);
+        if (color.a == 0) {
+          /// ignore alpha channel
+          continue;
+        }
         dst.setPixel(x + dstX, y + dstY, color);
       }
     }
